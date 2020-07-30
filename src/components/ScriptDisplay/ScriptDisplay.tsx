b7fc02fc86502590a1367eb0f01f3b4ef17ed609
Doug  Modie <dougmodie@MacBook-Pro.local>
2020-07-30 12:35:02 -0700

    creating new output links from menu node works now

--- src/components/ScriptDisplay/ScriptDisplay.tsx
+++ src/components/ScriptDisplay/ScriptDisplay.tsx
@@ -10,10 +10,14 @@ export interface ScriptDisplayProps {
 
 export const ScriptDisplay = (props: ScriptDisplayProps) => {
   const {chart }= props;
+
+  console.log('links in ScriptDisplay');
+  console.log(chart.links)
   const createScript = ()=>{
     const scriptObject = {}
     Object.keys(chart.links).map ( (key)=>{
       const originNode = chart.links[key].from.nodeId;
+      const originPort = chart.links[key].from.portId;
       const destNode = chart.links[key].to.nodeId;
       if (originNode && destNode){
         Object.keys(nodeInfo[originNode]).map ( (key0: any) => {
@@ -24,7 +28,29 @@ export const ScriptDisplay = (props: ScriptDisplayProps) => {
             scriptObject[originNode][key0] = nodeInfo[originNode][key0]
           }
         }) 
-        scriptObject[originNode].next = destNode;
+
+        console.log('updating script in script display')
+        console.log(originNode)
+        console.log(scriptObject)
+        console.log(scriptObject[originNode])
+        console.log(scriptObject[originNode][0])
+        // if menu node, set the next of the correct menu button 
+        // need to handle conditionals here as well
+        if (scriptObject[originNode].hasOwnProperty("menu")){
+          //node ports type
+          //get array of output ports
+          const arrayOutPorts = Object.keys(chart.nodes[originNode].ports).filter( (port)=> { return (chart.nodes[originNode].ports[port].type === 'output')} )
+          console.log('arrayOutPorts')
+          console.log(arrayOutPorts)
+          //get port
+          //find index of output port on node
+          const index = arrayOutPorts.indexOf(originPort);
+          scriptObject[originNode].menu[index].next = destNode;
+        }
+        else {
+          scriptObject[originNode].next = destNode;
+        }
+        
       }
     } )
     return (<div ><pre>{JSON.stringify(scriptObject, null, 2) }</pre></div>)
